d3bafd4920b306c5ee46d95cdd46e04f0f4a7563
JairoPJM <Jairomacassi@gmail.com>
2023-04-11 03:34:03 -0400

    more file structure

---
+++ src/components/About/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function About() {
+	return (
+		<section>
+			<div className="center" id="about">
+				<h1 className="page-header">About Me</h1>
+			</div>
+			<div className="center">
+				<img
+					src={require('../../assets/images/recent-photo.jpg')}
+					alt="about-me"
+					className="photo"
+				/>
+			</div>
+			<div>
+				<p>
+				Hey there, I'm Jairo, and I'm excited to share my portfolio 
+				webpage with you! Right now, I'm learning web development in 
+				a bootcamp program. In my free time, I run my own interior 
+				painting business. However, I've always been a fan of computers, 
+				and I've decided to change my career path and pursue web development 
+				full-time. It's something I'm passionate about, and I'm excited to 
+				see where this new journey takes me. Thank you for checking out my 
+				portfolio!
+				</p>
+			</div>
+		</section>
+	);
+}
+
+export default About;
